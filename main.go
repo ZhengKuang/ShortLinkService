f785d0cc23d94d2148c7fbdc5bc594d37d469e4a
ZhengKuang <kuangzheng2013212987@bupt.edu.cn>
2016-10-27 09:52:04 +0800

    add http handle mehtod

--- main.go
+++ main.go
@@ -18,8 +18,13 @@ var(
 func main(){
 	flag.StringVar(&port,"port",":8080","port to listen")
 	flag.Parse()
+
+	http.Handle("/hello/golang/",&BaseHandler{})
+	http.HandleFunc("/hello/world",func(resp  http.ResponseWriter, req *http.Request){
+		resp.Write([]byte("hello world"))
+	})
 	log.Println("ShortURL server will start at prot "+port)
-	log.Fatalln(http.ListenAndServe(port,&BaseHandler{}))
+	log.Fatalln(http.ListenAndServe(port,nil))
 }
 
 
