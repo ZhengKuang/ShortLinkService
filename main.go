1e977b29abd9ff7ee372c8a22a4ef42a59d04fec
ZhengKuang <kuangzheng2013212987@bupt.edu.cn>
2016-10-27 19:35:29 +0800

    add the use of contrller and config file

--- main.go
+++ main.go
@@ -1,44 +1,21 @@
 package main
 
-
 import (
-	"fmt"
-	"net/http"
 	"flag"
-	"log"
+	"fmt"
+	"short-url/controllers"
 
-
-	"short_url/lib/mux"
-
-
+	"github.com/astaxie/beego"
 )
 
-var(
+var (
 	port string
 )
 
-
-func main(){
-	flag.StringVar(&port,"port",":8080","port to listen")
+func main() {
+	flag.StringVar(&port, "port", ":8080", "port to listen")
 	flag.Parse()
 
-	router:=mux.NewMuxHandler()
-
-	router.Handle("/hello/golang/",&BaseHandler{})
-	router.HandleFunc("/hello/world",func(resp  http.ResponseWriter, req *http.Request){
-		resp.Write([]byte("hello world"))
-	})
-	log.Println("ShortURL server will start at prot "+port)
-	log.Fatalln(http.ListenAndServe(port,router))
-}
-
-
-type BaseHandler struct{
-
-}
-
-func (handler *BaseHandler) ServeHTTP(resp  http.ResponseWriter, req *http.Request){
-	fmt.Println("url path=>",req.URL.Path)
-	fmt.Println("url param a=>",req.URL.Query().Get("a"))
-	resp.Write([]byte("hello golang"))
+	beego.RESTRouter("api/url", &controllers.UrlController{})
+	beego.Run(port)
 }
