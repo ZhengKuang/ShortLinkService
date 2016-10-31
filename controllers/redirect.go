d7cb18536801f8f424103d9cb05b4ac461619e2d
ZhengKuang <kuangzheng2013212987@bupt.edu.cn>
2016-10-31 14:14:38 +0800

    modify controllers

--- controllers/redirect.go
+++ controllers/redirect.go
@@ -1,10 +1,34 @@
 package controllers
 
+import (
+	"fmt"
+	"short-url/models"
+	"short-url/utils"
+	"strings"
+)
+
 type RedirectController struct {
 	BaseController
 }
 
 func (this *RedirectController) Get() {
-	this.Redirect("/api/url", 302)
+	if this.Ctx.Request.URL.Path == "/" {
+		fmt.Println("RediretController has been used, the url is ", this.Ctx.Request.URL.Path)
+		this.Data["title_name"] = "short-link service_beego"
+		this.TplName = "index.html"
+		return
 
+	}
+	urlPath := this.Ctx.Request.URL.Path
+	urlPath = strings.Trim(urlPath, "/")
+	id := utils.StringToId(urlPath)
+	u := &models.Url{
+		Id: id,
+	}
+	err := u.FindById()
+	if err != nil {
+		this.error("no corresponding short URL in MongoDB:" + err.Error())
+		return
+	}
+	this.Redirect(u.SourceUrl, 302)
 }
--- controllers/url.go
+++ controllers/url.go
@@ -1,10 +1,27 @@
 package controllers
 
+import (
+	"net/url"
+	"short-url/models"
+	"short-url/utils"
+)
+
 type UrlController struct {
 	BaseController
 }
 
-func (c *UrlController) Get() {
-	c.success("dasfasdf")
-
+func (this *UrlController) Get() {
+	sourceUrl := this.Ctx.Input.Query("sourceUrl")
+	_, err := url.ParseRequestURI(sourceUrl)
+	if err != nil {
+		this.error("Url is not a valid url:" + err.Error())
+		return
+	}
+	u := &models.Url{
+		SourceUrl: sourceUrl,
+	}
+	u.GenId()
+	u.ShortUrl = utils.IdToString(u.Id)
+	u.Save()
+	this.success(u)
 }
